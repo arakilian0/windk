5bcd25000f31cfc522a4000d506112dcac113fa8
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-09-07 16:19:36 -0500

    refactor(windk/create): removing flag handlers entirely, simplifying for v0.1

--- tool/windk/command/create.bat
+++ tool/windk/command/create.bat
@@ -1,87 +1,18 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "TARGET_NAME="
-set "FLAGS="
+set "LEVELS=..\..\..\"
+set "NAME=%~1"
 
-: ==========
-: PARSE ARGS
-: ==========
-:parse_args
-if "%~1"=="" goto :main
+call "%~dp0%LEVELS%lib\ansi_codes.bat"
 
-:: Check Help Flags
-if /i "%~1"=="-h"     goto :show_help
-if /i "%~1"=="--help" goto :show_help
-if /i "%~1"=="help"   goto :show_help
-
-:: Extract 1st character to identify flags
-set "ARG=%~1"
-set "FIRST_CHAR=!ARG:~0,1!"
-
-if "!FIRST_CHAR!"=="-" goto :save_flag
-if "!FIRST_CHAR!"=="/" goto :save_flag
-
-:: --- POSITIONAL ARGUMENT HANDLING ---
-:: FIRST non-flag becomes TARGET_NAME (subsequent non-flags are ignored)
-if not defined TARGET_NAME (
-    set "TARGET_NAME=%~1"
-)
-shift
-goto :parse_args
-
-: =========
-: SAVE FLAG
-: =========
-:save_flag
-if defined FLAGS (
-    set "FLAGS=!FLAGS! "%~1""
-) else (
-    set "FLAGS="%~1""
-)
-shift
-goto :parse_args
-
-: ====
-: MAIN
-: ====
-:main
-if "!TARGET_NAME!"=="" (
-    echo [ERROR] No target name provided.
-    goto :show_help
+if "!NAME!" == "" (
+    echo error: please provide name of new cli.
+    goto end
 )
 
-:: Process flags to set state variables
-if defined FLAGS call :process_flags !FLAGS!
+echo hello "!NAME!"
+goto end
 
-:: --- LOG RESULTS ---
-echo ==========================================
-echo Target Name    : !TARGET_NAME!
-echo Verbose Mode   : !VERBOSE!
-echo Captured Flags : !FLAGS!
-echo ==========================================
-
-exit /b 0
-
-: =============
-: PROCESS FLAGS
-: =============
-:process_flags
-if "%~1"=="" exit /b 0
-
-if /i "%~1"=="-v"        set "VERBOSE=1"
-if /i "%~1"=="--verbose" set "VERBOSE=1"
-
-shift
-goto :process_flags
-
-: =========
-: SHOW HELP
-: =========
-:show_help
-echo Usage: %~nx0 ^<target_name^> [flags]
-echo.
-echo Example:
-echo   %~nx0 my_project -v
-echo   %~nx0 -v my_project
+:end
 exit /b 0
