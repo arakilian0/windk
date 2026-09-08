aa0e9ec2db9d1a1b33293547c6fae0b92a049623
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-09-07 21:36:59 -0500

    refactor(windk/create): simplify, logger

--- tool/windk/command/create.bat
+++ tool/windk/command/create.bat
@@ -1,87 +1,30 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "TARGET_NAME="
-set "FLAGS="
+set "HELP="
+set "FORCE="
+set "VERBOSE="
+set "TEMPLATE="
 
-: ==========
-: PARSE ARGS
-: ==========
 :parse_args
-if "%~1"=="" goto :main
+if "%~1" == "" goto main
 
-:: Check Help Flags
-if /i "%~1"=="-h"     goto :show_help
-if /i "%~1"=="--help" goto :show_help
-if /i "%~1"=="help"   goto :show_help
+if "%~1" == "-h" set "HELP=1"
+if "%~1" == "-f" set "FORCE=1"
+if "%~1" == "-v" set "VERBOSE=1"
+if "%~1" == "--template" set "TEMPLATE=%~2"
 
-:: Extract 1st character to identify flags
-set "ARG=%~1"
-set "FIRST_CHAR=!ARG:~0,1!"
-
-if "!FIRST_CHAR!"=="-" goto :save_flag
-if "!FIRST_CHAR!"=="/" goto :save_flag
-
-:: --- POSITIONAL ARGUMENT HANDLING ---
-:: FIRST non-flag becomes TARGET_NAME (subsequent non-flags are ignored)
-if not defined TARGET_NAME (
-    set "TARGET_NAME=%~1"
-)
 shift
-goto :parse_args
+goto parse_args
 
-: =========
-: SAVE FLAG
-: =========
-:save_flag
-if defined FLAGS (
-    set "FLAGS=!FLAGS! "%~1""
-) else (
-    set "FLAGS="%~1""
-)
-shift
-goto :parse_args
-
-: ====
-: MAIN
-: ====
 :main
-if "!TARGET_NAME!"=="" (
-    echo [ERROR] No target name provided.
-    goto :show_help
-)
+echo hello worldW
 
-:: Process flags to set state variables
-if defined FLAGS call :process_flags !FLAGS!
+echo help: !help!
+echo force: !force!
+echo verbose: !verbose!
+echo template: !template!
+goto end
 
-:: --- LOG RESULTS ---
-echo ==========================================
-echo Target Name    : !TARGET_NAME!
-echo Verbose Mode   : !VERBOSE!
-echo Captured Flags : !FLAGS!
-echo ==========================================
-
-exit /b 0
-
-: =============
-: PROCESS FLAGS
-: =============
-:process_flags
-if "%~1"=="" exit /b 0
-
-if /i "%~1"=="-v"        set "VERBOSE=1"
-if /i "%~1"=="--verbose" set "VERBOSE=1"
-
-shift
-goto :process_flags
-
-: =========
-: SHOW HELP
-: =========
-:show_help
-echo Usage: %~nx0 ^<target_name^> [flags]
-echo.
-echo Example:
-echo   %~nx0 my_project -v
-echo   %~nx0 -v my_project
+:end
 exit /b 0
