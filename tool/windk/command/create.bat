87387e8d6805ec03bb45aae0660872578c9cbc67
arakilian0 <31683477+arakilian0@users.noreply.github.com>
2026-09-07 23:38:26 -0500

    feat(windk/create): now uses template in lib/

--- tool/windk/command/create.bat
+++ tool/windk/command/create.bat
@@ -1,30 +1,46 @@
 @echo off
-setlocal EnableDelayedExpansion
 
-set "HELP="
-set "FORCE="
-set "VERBOSE="
-set "TEMPLATE="
+set "arg=%~1"
+set "LEVELS=..\..\..\"
+set "PATH_MGR=%~dp0%LEVELS%lib\path_resolver.bat"
+set "COLOR_MGR=%~dp0%LEVELS%lib\ansi_codes.bat"
+set "TEMPLATE_DIR=%~dp0%LEVELS%lib\__template"
 
-:parse_args
-if "%~1" == "" goto main
+call "%PATH_MGR%" resolve "%TEMPLATE_DIR%" TEMPLATE_DIR
+call "%COLOR_MGR%"
 
-if "%~1" == "-h" set "HELP=1"
-if "%~1" == "-f" set "FORCE=1"
-if "%~1" == "-v" set "VERBOSE=1"
-if "%~1" == "--template" set "TEMPLATE=%~2"
+if "%~1"=="" (
+    ECHO %RED%Error:%RESET% please provide the name of the script to %~n0.
+    GOTO end
+)
 
-shift
-goto parse_args
+where "$PATH:%~1" >nul 2>nul
 
-:main
-echo hello worldW
+if errorlevel 1 ( goto notfound ) else ( goto found )
 
-echo help: !help!
-echo force: !force!
-echo verbose: !verbose!
-echo template: !template!
+:found
+echo Error: Command ' %~1' already exists on system PATH. Please choose another name for your command.
+goto end
+
+:notfound
+:: Check if arg contains only letters and numbers
+echo !arg!| findstr /r "^[A-Za-z0-9]*$" >nul
+if errorlevel 1 (
+    echo Error: "%arg%" must contain only letters and numbers
+    exit /b 1
+)
+
+if not exist !TEMPLATE_DIR! (
+    echo Error: the template directory is missing.
+    exit /b 0
+)
+
+:: Main Process
+xcopy "%TEMPLATE_DIR%" "%~dp0%LEVELS%tool\!arg!" /E /I /H /R /Y /Q >nul 2>&1
+copy "%~dp0%LEVELS%bin\windk.bat" "%~dp0%LEVELS%bin\!arg!.bat" >nul
+move "%~dp0..\..\!arg!\main.bat" "%~dp0..\..\!arg!\!arg!.bat" >nul
+
 goto end
 
 :end
-exit /b 0
+endlocal
